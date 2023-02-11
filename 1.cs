892f5e03ddc3cc52276ad8c91430cf157f6cece7
LakhuroDiana <124384697+LakhuroDiana@users.noreply.github.com>
2023-02-11 12:09:09 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+if (N % 10 == N / 10000 || N / 1000 % 10 == N % 100 / 10)
+{
+    System.Console.WriteLine("-> Да");
+}
+else
+{
+    System.Console.WriteLine("-> Нет");
+}
