ab5aee7f02b4779f12f6a2450be68d4737e14021
Diana <kat.lahuro.3223@gmail.com>
2023-02-11 12:02:57 +0300

    3

---
+++ 3.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+int i=0;
+int k=1;
+int result=1;
+while (i<N)
+{
+    result=k;
+    System.Console.WriteLine(result*result*result);
+    i++;
+    k++;
+}
